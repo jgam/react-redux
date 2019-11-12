2c7446e37390b205948685de5d62171361d38536
jgam <jgam@alumni.nd.edu>
2019-11-12 09:16:36 +0900

    [init] web

---
+++ my-app/src/reducer/todos.js
@@ -0,0 +1,26 @@
+import todoAction from '../action/index';  
+const {ADD_TODO} = todoAction.todo;
+
+const todo = (state, action) => {  
+  switch (action.type) {
+    case ADD_TODO:
+      return {
+        text: action.text,
+        completed: false
+      };
+    default:
+      return state;
+  }
+}
+
+
+const todos = (state = [], action) => {  
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state, todo(undefined, action)
+      ];
+    default:
+      return state;
+  }
+}
